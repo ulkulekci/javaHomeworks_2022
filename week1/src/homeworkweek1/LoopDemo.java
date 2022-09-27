686b894a6abae5d4908440c4322a1ef7d8972edc
ulkulekci <ulkusulu07@hotmail.com>
2022-09-27 18:04:11 +0300

    for döngüsü

---
+++ week1/src/homeworkweek1/LoopDemo.java
@@ -0,0 +1,15 @@
+package homeworkweek1;
+
+import java.util.Iterator;
+
+public class LoopDemo {
+	
+	public static void main(String[] args) {
+		//For i+=2 2 arttır
+		for(int i=1;i<=10;i++) {
+			System.out.println(i);
+		}
+		System.out.println("Döngü Bitti");
+	}
+	
+}
