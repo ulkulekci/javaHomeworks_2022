659b90e54de88f034fd6167be8bfeb6cf9288443
ulkulekci <ulkusulu07@hotmail.com>
2022-09-27 17:22:37 +0300

    1.hafta ödevleri

---
+++ week1/src/homeworkweek1/Conditionals.java
@@ -0,0 +1,24 @@
+package homeworkweek1;
+
+public class Conditionals {
+	
+	public static void main(String[] args) {
+		
+		int sayi = 24;
+	    if (sayi<20) {
+			System.out.println("Sayı 20'den küçüktür");
+		}
+	    
+	    //if (sayi<15) {
+	    //System.out.println("Sayı 15'den küçüktür");
+		//}
+	    
+	    else if(sayi==20){
+			System.out.println("Sayı 20'ye eşittir");
+		}
+	    else {
+	    	System.out.println("Sayı 20'den büyüktür");
+		}
+	}
+	
+}
---
+++ week1/src/homeworkweek1/Homework1_2.java
@@ -0,0 +1,23 @@
+package homeworkweek1;
+
+public class Homework1_2 {
+	/*
+	 * Ödev 1
+	 * Trendyol sitesine girdiğiniz zaman bu sitede kullanılan şartlı bloklara ve
+	 * döngülerle yapılmış alanlara örnekler bulunuz.
+	 * Görsellerle işaretleyip belirtiniz.
+	 * ürünlerin sıra ile açılması döngü..., farklı farklı bölümler ör kadın erkek
+	 * giriş ençok kullanılanlarda.... şart blokları ile yapılmıştır.
+	 * Ödev 2
+	 * https://www.youtube.com/watch?v=a8Fe2qbnYDM&list=
+	 * PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=2
+	 * Bu oynatma listesinde 1-23 arasındaki tüm dersleri izleyip uygulayınız.(1-23
+	 * dahil) Intellij, netbeans veyaeclipse kullanabilirsiniz.
+	 * Uyguladığınız kodları github'a aktarınız.
+	 * Github'a aktarmak için discorddan destek isteyiniz.
+	 * Tüm kodların uygulanmasını bekliyoruz.
+	 * Github adresinizi yorumlara ekleyiniz.
+	 * Diğer arkadaşlarınızın kodlarını inceleyiniz.
+	 * Beğenirseniz yıldızlayınız.
+	 */
+}
