3a52cf8c06023b64153670e4c1e87785cc8de21d
ulkulekci <ulkusulu07@hotmail.com>
2022-09-27 19:58:51 +0300

    For ve while döngüsü

--- week1/src/homeworkweek1/LoopDemo.java
+++ week1/src/homeworkweek1/LoopDemo.java
@@ -9,7 +9,14 @@ public class LoopDemo {
 		for(int i=1;i<=10;i++) {
 			System.out.println(i);
 		}
-		System.out.println("Döngü Bitti");
-	}
-	
+		System.out.println("For Döngüsü Bitti");
+		int i=1;
+		//While
+		while(i<10) {
+		System.out.println(i);
+		i++;
+
+		}
+		System.out.println("While Döngüsü Bitti");
+	}	
 }
