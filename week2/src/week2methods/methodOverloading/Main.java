5523bc131b4f19d0346f13f6d072fb22c62cca3e
UlkuKulekci <87547007+ulkulekci@users.noreply.github.com>
2022-10-04 00:06:11 +0300

    2.hafta odevleri

---
+++ week2/src/week2methods/methodOverloading/Main.java
@@ -0,0 +1,13 @@
+package methodOverloading;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		System.out.println(dortIslem.topla(3,4));
+		dortIslem.topla(2,3,4);
+		
+	}		
+	}
+	
+
