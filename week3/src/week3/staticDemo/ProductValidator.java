a99caaebd45c4487c6798eec5b72444d15b7c16d
ulkulekci <ulkusulu07@hotmail.com>
2022-10-09 19:29:04 +0300

    Composition Inner Class ve Static

--- week3/src/week3/staticDemo/ProductValidator.java
+++ week3/src/week3/staticDemo/ProductValidator.java
@@ -3,8 +3,15 @@ package week3.staticDemo;
 public class ProductValidator {
 	public static boolean isValid(Product product) {
 		if(product.price > 0 && !product.name.isEmpty())
-			return true;
+			{return true;}
 		else
-			return false;
+			return false;}
+		public void bisey() {
+			
+		}
+		public static class BaskaBirClass{
+			public static void sil() {
+
+		}
 	}
 }
